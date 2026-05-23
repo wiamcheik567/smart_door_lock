806938fbc306cc1b046d0d7b7e5070c622d3ab63
wiamch567 <wiamcheikh176@gmail.com>
2026-05-24 00:03:02 +0100

    Clear MQTT credentials for security reasons
    Removed sensitive MQTT broker credentials from the sketch.

--- smart_door_lock/wokwi/code_vulnerable/sketch.ino
+++ smart_door_lock/wokwi/code_vulnerable/sketch.ino
@@ -12,14 +12,14 @@
 const char* WIFI_SSID     = "Wokwi-GUEST"; 
 const char* WIFI_PASSWORD = "";          
 // ── EMQX Cloud (MQTT over TLS) Configuration ────────────────────────
-const char* MQTT_BROKER   = "yf4fb665.ala.eu-central-1.emqxsl.com";
+const char* MQTT_BROKER   = "";
 const int   MQTT_PORT     = 8883;
 const char* TOPIC_STATUS  = "door/status";
 const char* TOPIC_COMMAND = "door/command";
 
 
-const char* MQTT_USER = "smart_lock";
-const char* MQTT_PASS = "IoT2000****"; 
+const char* MQTT_USER = "";
+const char* MQTT_PASS = ""; 
 
 WiFiClientSecure wifiClient;
 PubSubClient     mqttClient(wifiClient);
